3862bedc16d7a6c4aa85cfb4d65413f78419c3bb
paolo coca <mrpaolo419@gmail.com>
2024-11-09 19:50:08 -0300

    gif

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import ChecKout from "./component/pages/checkout/ChecKout";
 
 import ItemDetailContainer from "./component/pages/itemDetailContainer/ItemDetailContainer";
 import { CartContextProvider } from "./component/context/CartContext";
-
+//nuevo gif
 function App() {
   return (
     <BrowserRouter>
